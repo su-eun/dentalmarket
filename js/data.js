4a9f89a6fff50f843295a22339e23333662d857e
su-eun <58408937+su-eun@users.noreply.github.com>
2019-12-24 11:08:59 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,74 @@
+// data.js
+
+/** 제품 정보 데이터(JSON) */
+var product_data = [
+  {
+    // 속성: 값
+    img_url: "images/item1.jpg",
+    title: "DentiAnn Gauze (2 x 2 inch)",
+    name: "덴티안 거즈",
+    origin_price: "30,000",
+    sale_price: "회원가: 20,000",
+    category: "man",
+},
+{    // 속성: 값
+    img_url: "images/item5.jpg",
+    title: "Osung",
+    name: "Periosteal elevator",
+    origin_price: "148,000",
+    sale_price: "회원가: 100,000",
+    category: "woman",
+},
+{
+    // 속성: 값
+    img_url: "images/item2.jpg",
+    title: "Botulinum Toxin Type A-개별",
+    name: "보툴렉스 50U",
+    origin_price: "40,000",
+    sale_price: "회원가: 35,000",
+    category: "man",
+},
+{
+    // 속성: 값
+    img_url: "images/item3.jpg",
+    title: "Aroma Fine Pluse",
+    name: "알지네이트(10ea)",
+    origin_price: "100,000",
+    sale_price: "회원가: 94,000",
+    category: "man",
+},
+{    // 속성: 값
+    img_url: "images/item6.jpg",
+    title: "Dental studio",
+    name: "Surgical curette",
+    origin_price: "60,000",
+    sale_price: "회원가: 53,000",
+    category: "woman",
+},
+{    // 속성: 값
+    img_url: "images/item8.jpg",
+    title: "Seil Global",
+    name: "Diamond bur",
+    origin_price: "99,000",
+    sale_price: "회원가: 82,000",
+    category: "woman",
+},
+{
+    // 속성: 값
+    img_url: "images/item4.jpg",
+    title: "BENEFIT Latex Glove",
+    name: "라텍스 글러브(파우더프리)",
+    origin_price: "80,000",
+    sale_price: "회원가: 79,000",
+    category: "man",
+},
+{    // 속성: 값
+    img_url: "images/item7.jpg",
+    title: "Komet",
+    name: "Bone curette",
+    origin_price: "90,000",
+    sale_price: "회원가: 82,000",
+    category: "woman",
+},
+];
+
